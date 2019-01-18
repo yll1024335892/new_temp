64ccc92d002cedf6aa1dbd899e4b2f683826d800
yll1024335892 <yll1024335892@163.com>
2019-01-18 08:43:51 +0800

    确定提交

--- config.rb
+++ config.rb
@@ -10,12 +10,12 @@ javascripts_dir = "javascripts"
 
 # 输出边缘成css的格式:
 # output_style = :expanded or :nested or :compact or :compressed
-
+output_style = :compact
 # 是否取消compass的注释:
-# relative_assets = true
+ relative_assets = true
 
 # 禁用显示选择器原始位置的调试注释，取消注释:
-# line_comments = false
+ line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
